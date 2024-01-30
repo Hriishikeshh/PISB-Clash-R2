0195964075c3b7ce22b759793062f948f690d9d0
Hrishikesh Patkar <patkarhrishikesh0204@gmail.com>
2024-01-30 18:18:55 +0530

    commit rec

--- src/components/Qpanel.js
+++ src/components/Qpanel.js
@@ -1,10 +1,13 @@
 import React from 'react'
-import Card from "./Card"
 
 function Qpanel() {
   return (
-    <div className="box-border pt-12 flex-col items-center justify-center font-bold text-white w-80">
-          <Card />
+    <div className="box-border flex items-center justify-center font-bold text-white mr-80">
+        <div className='border-solid border-4 border-black rounded-xl flex-col m-5 w-screen'>
+        <h1 className='m-2'>Card title</h1>
+        <p className='m-2 text-base text-center mb-32'>What song would make the best theme music for you?</p>
+        <button class="h-10 w-20 relative inline-flex items-center justify-center ml-2 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-300 to-blue-600 group-hover:from-green-100 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-20">submit</button>
+      </div>
     </div>
     
   )
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,8 +3,8 @@ import React from 'react'
 function Navbar() {
   return (
     <div>
-        <header className='fixed top-0 left-0 w-full h-16 p-y-full p-x-20 flex justify-between items-center z-100'>
-            <a href="#" className='px-10 text-3xl font-bold text-white'>Credenz.</a>
+        <header className='top-0 left-0 w-full h-16 p-y-full p-x-20 flex justify-between items-center z-100'>
+            <a href="#" className='px-10 text-3xl font-bold text-white drop-shadow-xl'>Credenz.</a>
 
             <nav className='text-base text-white font-medium m-20'>
                 <a href="#" className='p-2 transition duration-0.3 hover:text-black'>Home</a>
